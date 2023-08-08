8c2d91d4688f8b5f3a6878cb740171f46dabb1c6
Vinicius <vinicius.camargo.bandeira@gmail.com>
2023-08-08 03:18:47 -0300

    criando a interface do menu

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuItems = document.querySelectorAll('.menu-item');
+
+menuItems.forEach(item => {
+
+    item.addEventListener('click', () => {
+        item.classList.toggle('active');
+    });
+
+}
+);
